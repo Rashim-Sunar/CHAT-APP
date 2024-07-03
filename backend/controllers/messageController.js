d5f62443617e57ba3075a9697f5556ee52c58285
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-07-03 16:20:43 -0400

    Saving the conversation in database

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -34,6 +34,11 @@ export const sendMessage = async(req, res) => {
         conversation.messages.push(newMessage._id);
     }
       
+    // await newMessage.save();
+    // await conversation.save();
+
+    await Promise.all([newMessage.save(), conversation.save()]);
+
     res.status(201).json({
         status: 'success',
         newMessage
