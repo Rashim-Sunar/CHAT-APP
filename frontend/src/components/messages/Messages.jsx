20a7e27ea938db45185d42af97b7703efcac3316
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-07-05 15:27:31 -0400

    Message component completed

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Message from './Message.jsx'
+
+const Messages = () => {
+  return (
+    // overflow-auto -->When messages are many then there seem a scroll bar...
+    <div className='px-4 flex-1 overflow-auto'> 
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+    </div>
+  )
+}
+
+export default Messages
