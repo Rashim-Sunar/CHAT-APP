25f682c9f7c048922efc22eaca05d52fbf378727
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-07-05 05:11:48 -0400

    Sidebar of the chatbox created excluding logout button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import "./index.css"
 import Login from "./pages/login/Login.jsx"
 import Signup from "./pages/signup/Signup.jsx"
+import Home from "./pages/home/Home.jsx"
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
         {/* <Login/>   */}
-        <Signup/>
+        {/* <Signup/> */}
+        <Home/>
     </div>
   )
 }
