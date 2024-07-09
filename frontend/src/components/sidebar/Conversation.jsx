6ca7eb0782c0035e964c6a8d54f0ac8bd6bc148c
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-07-09 14:04:50 -0400

    Unsing zustand - a state management to keep the track of which user is selected to chat

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,7 +1,13 @@
+import useGetConversation from "../../zustand/useConversation";
+
 const Conversation = ({conversation, emoji, lastIndex}) => {
+	const {selectedConversation, setSelectedConversation} = useGetConversation();
+
+	const isSelected = selectedConversation?._id === conversation._id;
 	return (
 		<>
-			<div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+			<div className={`flex gap-2 items-center hover:bg-sky-800 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-sky-500" : ""}`} onClick={()=>setSelectedConversation(conversation)}>
 				<div className='avatar online'>
 					<div className='w-12 rounded-full'>
 						<img
