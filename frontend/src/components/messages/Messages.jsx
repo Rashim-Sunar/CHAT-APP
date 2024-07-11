f55dbb6a9c30cfe1323cb3a4786023bdc1c5af93
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-07-11 12:23:06 -0400

    Adding functionality to search user now onwards we will be dealing with socket.io

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message.jsx'
 import useGetMessages from '../../hooks/useGetMessages.js'
 import MessageSkeleton from '../skeleton/MessageSkeleton.jsx';
@@ -6,11 +6,21 @@ import MessageSkeleton from '../skeleton/MessageSkeleton.jsx';
 const Messages = () => {
   const {loading, messages} = useGetMessages();
   // console.log(messages);
+  const lastMessageRef = useRef();
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      lastMessageRef.current?.scrollIntoView({behavior: "smooth"});
+    }, 100);
+  },[messages]);
+
   return (
     // overflow-auto -->When messages are many then there seem a scroll bar...
     <div className='px-4 flex-1 overflow-auto'> 
       {!loading && messages.length > 0 && messages.map((message) => (
-        <Message key={message._id} message={message}/>
+        <div key={message._id} ref={lastMessageRef}>
+            <Message message={message}/>
+        </div>
       ))}
 
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx}/> )}
