f9e7313ec5ce59530244f90c53456d007b4bed32
Rashim-Sunar <rashimsunar.02@gmail.com>
2026-07-14 23:27:47 +0530

    fix(eslint): enable typescript-aware linting and fix surfaced issues

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,19 +3,30 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true,
+        // Context provider files idiomatically co-export their consumer hook;
+        // that hook export doesn't break Fast Refresh, only warn about the rest.
+        allowExportNames: [
+          'useAuthContext',
+          'useSocketContext',
+          'useDeviceLinkContext',
+        ],
+      },
     ],
   },
 }
